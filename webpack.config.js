fbb4b3afe61bc01f3269a16d7af5545537e2a343
Jbeezy18 <justinpoeh@gmail.com>
2024-02-02 16:45:22 -0800

    added dependOn to webpack.conf and fixed type in another-module.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,15 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
   output: {
     filename: '[name].bundle.js',
