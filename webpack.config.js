dcf2ae246f1bf9e794b93fd5665da2e690b3dfc6
Jbeezy18 <justinpoeh@gmail.com>
2024-02-02 16:35:18 -0800

    changed mode to development and changed input and output module exports

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    another: './src/another-module.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
+  },  
 };
